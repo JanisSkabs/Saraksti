d5c2315a683926ebe45aa00d2b1b0d2e3226f7b6
Jānis Skabs <94110515+JanisSkabs@users.noreply.github.com>
2021-12-09 10:42:26 +0000

    uzd2

--- main.py
+++ main.py
@@ -30,29 +30,29 @@ def uzdevums1():
 
 def uzdevums2():
   print('Uzdevums2')
-a = int(input("Ievadi 1. skaitli, kurš ir intervālā no -10 līdz 10"))
-b = int(input("Ievadi 2. skaitli, kurš ir intervālā no -10 līdz 10"))
-c = int(input("Ievadi 3. skaitli, kurš ir intervālā no -10 līdz 10"))
-d = int(input("Ievadi 4. skaitli, kurs ir intervālā no -10 līdz 10"))
-e = int(input("Ievadi 5. skaitli, kurs ir intervālā no -10 līdz 10"))
-print( a, b, c, d, e)
-z=0
-if a < b:
-  print(" 2. ievadītais skaitlis ir lielāks ar 1. ievadīto skaitli")
-  z=z+1
+  a = int(input("Ievadi 1. skaitli, kurš ir intervālā no -10 līdz 10"))
+  b = int(input("Ievadi 2. skaitli, kurš ir intervālā no -10 līdz 10"))
+  c = int(input("Ievadi 3. skaitli, kurš ir intervālā no -10 līdz 10"))
+  d = int(input("Ievadi 4. skaitli, kurs ir intervālā no -10 līdz 10"))
+  e = int(input("Ievadi 5. skaitli, kurs ir intervālā no -10 līdz 10"))
+  print( a, b, c, d, e)
+  z=0
+  if a < b:
+    print(" 2. ievadītais skaitlis ir lielāks ar 1. ievadīto skaitli")
+    z=z+1
 
-if b<c:
-  print(" 3. ievadītais skaitlis ir lielāks ar 2. ievadīto skaitli")
-  z=z+1  
+  if b<c:
+    print(" 3. ievadītais skaitlis ir lielāks ar 2. ievadīto skaitli")
+    z=z+1  
 
-if c < d:
-  print(" 4. ievadītais skaitlis ir lielāks ar 3. ievadīto skaitli")  
-  z=z+1
+  if c < d:
+    print(" 4. ievadītais skaitlis ir lielāks ar 3. ievadīto skaitli")  
+    z=z+1
 
-if d < e:
-  print(" 5. ievadītais skaitlis ir lielāks ar 4. ievadīto skaitli") 
-  z=z+1 
-print("Pēc skaita sarakstā skaitļi, kuri lielāki par iepriekšpēdējo saraksta elementu ir:  ",z)
+  if d < e:
+    print(" 5. ievadītais skaitlis ir lielāks ar 4. ievadīto skaitli") 
+    z=z+1 
+  print("Pēc skaita sarakstā skaitļi, kuri lielāki par iepriekšpēdējo saraksta elementu ir:  ",z)
 
 
 def uzdevums3():
